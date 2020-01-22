cbb1e1e2d615e8fbd06bc2496d8e43540bb81064
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2020-01-21 18:49:54 -0800

    Update build/* files

--- build/index.js
+++ build/index.js
@@ -4,12 +4,11 @@
  * Author: Joshua Carter
  * Created: July 4, 2017
  */
-"use strict";
-//include modules
+"use strict"; //include modules
 
 var _Ops = require("./Ops.js");
 
 //create ops instance
-var instance = new _Ops.Ops();
-//export modules
+var instance = new _Ops.Ops(); //export modules
+
 module.exports = instance;
